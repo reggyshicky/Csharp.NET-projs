5c229056996fe73be8f172d14e4b236672aad697
Reggy Shicky <reginahshikanda@gmail.com>
2023-09-02 20:38:37 +0300

    Configure the relationship between Actor_Movie and Movie Configure the relationship between Actor_Movie and Actor in the AppDbContext.cd

--- eTickets/eTickets/Data/AppDbContext.cs
+++ eTickets/eTickets/Data/AppDbContext.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
-using  eTickets.Models;
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace eTickets.Data
 {
     public class AppDbContext : DbContext
@@ -9,8 +10,24 @@ namespace eTickets.Data
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //modelBuilder.Entity<Actor_Movie>().HasKey(am => )
+            //// Define the composite primary key for the Actor_Movie table
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+            });
+            // Configure the relationship between Actor_Movie and Movie
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.MovieId);
+            // Configure the relationship between Actor_Movie and Actor
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.ActorId);  
+
             base.OnModelCreating(modelBuilder);
         }
+        //define table names for each model
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
     }
 }
--- eTickets/eTickets/Models/Actor.cs
+++ eTickets/eTickets/Models/Actor.cs
@@ -11,6 +11,6 @@ namespace eTickets.Models
         public string Bio { get; set; }
 
         //Relationships
-        public List<Movie> Actors_Movies{ get; set; }
+        public List<Actor_Movie> Actors_Movies{ get; set; }
     }
 }
