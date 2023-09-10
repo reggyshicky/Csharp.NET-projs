5c8c7aff855ea9119216ceb1076fa136413d0949
Reggy Shicky <reginahshikanda@gmail.com>
2023-09-10 17:09:06 +0300

    CRUD operations complete, working on deploymet

--- BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -34,13 +34,116 @@ namespace BulkyBookWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (obj.Name==obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid) //checks whether Name and DisplayOrder are empty or not
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created succesfully";
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+            
         }
 
+
+        
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var CategoryFromDb = _db.Categories.Find(id);
+           // var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+           // var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id); three ways to retrieve a category from the database based on the id
+            
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(CategoryFromDb);
+        }
+
+        //Post action method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid) //checks whether Name and DisplayOrder are empty or not
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated succesfully";
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
+        }
+        //GET method
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var CategoryFromDb = _db.Categories.Find(id);
+            // var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            // var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id); three ways to retrieve a category from the database based on the id
+
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(CategoryFromDb);
+        }
+
+        //Post action method
+        [HttpPost,ActionName("Delete")] //You can explicitly give your method another name, modify in the views too
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id); 
+            if (obj == null)
+            { 
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted succesfully";
+            return RedirectToAction("Index");
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
  * _db.Categories:
 _db is an instance of the ApplicationDbContext class, which represents the database context for your application.
--- BulkyBook/BulkyBookWeb/Models/Category.cs
+++ BulkyBook/BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
@@ -9,6 +9,8 @@ namespace BulkyBookWeb.Models
         public int Id { get; set; }
         [Required] //makes sure name is not a nullable property
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage ="Display Order must be between 1 and 100!!")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
