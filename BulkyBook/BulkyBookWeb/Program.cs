89c28512c172077b29678c11aae0cf5c45048387
Reggy Shicky <reginahshikanda@gmail.com>
2023-09-11 11:03:08 +0300

    https://bulkbookwebb.azurewebsites.net/Category

--- BulkyBook/BulkyBookWeb/Program.cs
+++ BulkyBook/BulkyBookWeb/Program.cs
@@ -8,18 +8,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
+    options.UseSqlServer(builder.Configuration["DbConnection"]));
 //builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-
+//UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+
 }
-app.UseExceptionHandler("/Home/Error");
-// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-app.UseHsts();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
